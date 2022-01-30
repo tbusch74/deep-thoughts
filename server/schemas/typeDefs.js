b8072bf484b6ad8076e5f715cc1198f1bde4afa1
Tony <abusch@css.edu>
2022-01-30 16:24:52 -0600

    final

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -40,7 +40,10 @@ type Query {
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-}
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 
 type Auth {
     token: ID!
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -38,4 +39,13 @@ db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
+});
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
